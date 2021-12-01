5591617e32e699e4256f27c0e392dc7d7f81d178
benitogarcia <jose.benito.garcia.solano@gmail.com>
2021-11-30 19:49:03 -0600

    Se agrego archivos faltantes

---
+++ code-config-server/src/test/java/com/tutorial/codeconfigserver/CodeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.codeconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
